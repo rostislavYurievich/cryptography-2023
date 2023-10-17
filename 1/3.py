971f2fb49617298285c404fbefcea762dbf6cfa9
Rostislav Yurievich <rostislav@yurievich.ru>
2023-10-17 12:28:22 +0300

    Lab 1 Task 3 done

--- 1/3.py
+++ 1/3.py
@@ -1,10 +1,10 @@
 MIN = 32
 MAX = 127
 
+RAND_MAX = 20
+
 from random import randint
 
-MIN = 32
-MAX = 127
 
 def shift(ch, shift_num):
     if shift_num<0:
@@ -12,13 +12,12 @@ def shift(ch, shift_num):
     return chr((ord(ch)-MIN+shift_num)%(MAX-MIN)+MIN)
 
 def cypher(string,shift_num):
-    return [shift(x, shift_num+(MAX-MIN)*randint(0,10)) for x in string]
+    return [ord(shift(x, shift_num))+(MAX-MIN)*randint(1,RAND_MAX) for x in string]
 
 def decypher(l, shift_num):
     return "".join([shift(chr(x%(MAX-MIN)), -shift_num) for x in l])
     
-#a = input("Введите шифруемую строку ")
-a = "Hello, World!"
+a = input("Введите шифруемую строку ")
 a1 = cypher(a,6)
 a2 = decypher(a1,6)
 print("Зашифрованная строка {a1}\nРазшифрованная строка {a2}".format(a1=a1, a2=a2))
