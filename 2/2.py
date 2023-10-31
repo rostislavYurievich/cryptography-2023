107a5eda66e8c49e1e9d4aa2efbeefb8b87d9701
Rostislav Yurievich <rostislav@yurievich.ru>
2023-10-31 13:32:29 +0300

    fixed 2.2

--- 2/2.py
+++ 2/2.py
@@ -3,7 +3,7 @@ from itertools import cycle
 def one_time_pad_encrypt(message, key):
     encrypted = []
     for c,k in zip(message,cycle(key)):
-        encrypted_byte = c + k % 26
+        encrypted_byte = c ^ k
         encrypted.append(encrypted_byte)
     return bytes(encrypted)
 
@@ -22,4 +22,4 @@ ciphertext = one_time_pad_encrypt(plaintext.encode('utf-8'), key.encode('utf-8')
 decrypted_text = one_time_pad_decrypt(ciphertext, key.encode('utf-8'))
 
 print("\nЗашифрованный текст (в байтах):\n", ciphertext)
-print("\nДешифрованный текст на русском:\n", decrypted_text.decode('utf-8'))
+print("\nДешифрованный текст на русском:\n", decrypted_text.decode())
--- 4/1.py
+++ 4/1.py
@@ -1,53 +1,35 @@
 import random
 import sympy
 import math
-# Функция для проверки на простоту
-def isprime(num):
-    for n in range(2,int(num**0.5)+1):
-        if num%n==0:
-            return False
-    return True
-
-# Функция для нахождения обратного модуля
-def mod_inverse(a, m):
-    m0, x0, x1 = m, 0, 1
-    while a > 1:
-        q = a // m
-        m, a = a % m, m
-        x0, x1 = x1 - q * x0, x0
-    return x1 + m0 if x1 < 0 else x1
 
 # Генерация ключей
-def generate_keys(bit_length):
+def generate_keys():
     # Генерация случайных простых чисел p и q
-    p = sympy.randprime(2 ** (bit_length - 1), 2 ** bit_length - 1)
-    q = sympy.randprime(2 ** (bit_length - 1), 2 ** bit_length - 1)
+    p = sympy.randprime(1,1000)
+    q = sympy.randprime(1,1000)
 
     n = p * q
     phi = (p - 1) * (q - 1)
 
-    e = 65537  # Обычно выбирают фиксированное значение для e
-
-    # Нахождение обратного модуля для e
-    d = mod_inverse(e, phi)
+    e = 2
+    while (math.gcd(e,phi)!=1):
+        e = sympy.randprime(1,phi-1)
+    d = (k*phi(n) + 1)/e
     return ((e, n), (d, n))
 
 # Шифрование
 def encrypt(public_key, plaintext):
     e, n = public_key
-    cipher_text = [pow(ord(char), e, n) for char in plaintext]
-    return cipher_text
+    return [pow(ord(char), e)%n for char in plaintext]
 
 # Дешифрование
 def decrypt(private_key, cipher_text):
     d, n = private_key
-    plain_text = [chr(pow(char, d, n)) for char in cipher_text]
-    return ''.join(plain_text)
+    return ''.join([chr(pow(char, d)%n) for char in cipher_text])
 
-bit_length = 256  # Длина ключа в битах
 
 # Генерация ключей
-public_key, private_key = generate_keys(bit_length)
+public_key, private_key = generate_keys()
 
 # Сообщение для шифрования
 message = "Hello, RSA!"
